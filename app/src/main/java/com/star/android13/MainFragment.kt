a0d510f3ab8f961dda621cfad9631160cb4d58f0
shixingxing <shixingxing@yungengxin.com>
2023-08-16 17:12:32 +0800

    通知权限

--- app/src/main/java/com/star/android13/MainFragment.kt
+++ app/src/main/java/com/star/android13/MainFragment.kt
@@ -1,9 +1,16 @@
 package com.star.android13
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.star.android13.databinding.FragmentMainBinding
@@ -16,6 +23,12 @@ class MainFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        if (it) {
+            postNotification()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,10 +46,43 @@ class MainFragment : Fragment() {
         binding.button1.setOnClickListener {
             findNavController().navigate(R.id.action_to_MediaFragment)
         }
+        binding.button2.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            } else {
+                postNotification()
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun postNotification() {
+        context?.let {
+            val notificationManager: NotificationManager =
+                it.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+
+            val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel("channel_id", "channel_name", NotificationManager.IMPORTANCE_LOW)
+                notificationManager.createNotificationChannel(channel)
+                NotificationCompat.Builder(
+                    it,
+                    channel.id
+                )
+            } else {
+                NotificationCompat.Builder(it)
+            }
+            notification.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("通知标题")
+                .setContentText("通知文本")
+
+            notificationManager.notify(2, notification.build())
+
+
+        }
+
     }
 }
